294299c17adc98ff98ea983310fb60cf8e72bd39
temchukwu <godblesschuks@yahoo.com>
2022-05-15 15:06:07 +0100

    chukso

---
+++ chuksoo.js
@@ -0,0 +1,20 @@
+
+function add(x, y) {
+    return (x+y);
+}
+console.log(5, 6);
+
+let person = {
+    name: "chuks",
+    height: 56,
+    complexion: "fair",
+    age: 14
+}
+console.log(Object.keys(person));
+console.log(Object.values(person));
+let result = JSON.stringify(person)
+console.log(person);
+
+
+
+ 
